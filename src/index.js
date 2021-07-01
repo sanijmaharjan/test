b9e0667e8e445abb3f82c3cdfbc803142bb2018f
sanij <snz.mhrzn@gmail.com>
2021-07-01 16:38:12 +0545

    new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import "./index.css";
+import App from "./components/App/App.jsx";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, document.getElementById("root"));
